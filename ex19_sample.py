03726f4c534c736b5070d0d05d4c2e812bb12152
Madvesha Hebbar <41053550+madveshahebbar@users.noreply.github.com>
2018-08-31 17:01:53 +0530

    Add files via upload

---
+++ ex19_sample.py
@@ -0,0 +1,11 @@
+#EX19EX19_Sample_program
+def users (first_user, second_user):
+    print (f" First username is: {first_user}")
+    print (f" Second username is: {second_user}")
+
+print (f"Enter first user name")
+first_user=input()
+print (f"Enter second user name")
+second_user=input()
+
+users (first_user, second_user)
